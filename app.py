637f485953bf42870d6fff6508bcef26fc178137
Fernando Mayo <fernando@undefinedlabs.com>
2019-04-01 15:52:30 +0200

    Add /car/<uuid> route

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+import random
+from uuid import UUID
+
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/car/<car_uuid>")
+def car(car_uuid):
+    return jsonify({
+        'uuid': str(UUID(car_uuid)),
+        'lat': random.uniform(-90, 90),
+        'lon': random.uniform(-180, 180),
+    })
